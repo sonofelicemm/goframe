8771b732452e7cd879fe72e0d632f3d8d8219b4c
zhangmengmeng01 <zhangmengmeng01@baidu.com>
2018-05-10 18:47:55 +0800

    add new frame
    add new frame

---
+++ nlu-server/src/nlu/dao/mysql.go
@@ -0,0 +1,24 @@
+package dao
+
+import (
+	"database/sql"
+	"nlu/log"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Dao .
+type Dao struct {
+	db *sql.DB
+}
+
+// New .
+func New() (d *Dao) {
+	var err error
+	d = &Dao{}
+	d.db, err = sql.Open("mysql", "127.0.0.1:3306")
+
+	if err != nil {
+		log.Error("sql.Open()", err)
+	}
+	return
+}
